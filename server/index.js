4eb4e54c961d7a12bf33e8557c79a459bb6edd99
ArunSiva <Arunsiva003@users.noreply.github.com>
2023-05-25 21:52:07 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import clientRoutes from "./routes/client.js"
+import generalRoutes from "./routes/general.js"
+import managementRoutes from "./routes/management.js"
+import salesRoutes from "./routes/sales.js"
+
+dotenv.config()
+
+const app=express();
+app.use(express.json())
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(cors())
+
+app.use("./client",clientRoutes);
+app.use("./general",generalRoutes);
+app.use("./management",managementRoutes);
+app.use("./sales",salesRoutes);
+
+// mongoose Setup 
+
+const PORT = process.env.PORT || 9000;
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology:true,
+}).then(()=>{
+    app.listen(PORT,()=>console.log(`Mongodb connected port:${PORT}`))
+}).catch((err) => {
+    console.log(`${err} didnot connect`)
+})
